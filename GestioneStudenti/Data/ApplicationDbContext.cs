f2c86d98f66045a7d69851273ebfdb932bc960a2
JustLoryy <74098380+JustLoryy@users.noreply.github.com>
2022-04-27 20:01:13 +0200

    Added Migrations and ApplicationDbContext

---
+++ GestioneStudenti/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestioneStudenti.Services;
+
+namespace GestioneStudenti.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
