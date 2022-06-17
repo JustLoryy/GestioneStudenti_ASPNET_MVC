a9e4d59a1926234e5871ba19b8ddf72a38e71fb9
JustLoryy <lorenzo.ribel.battistoni@gmail.com>
2022-06-18 00:48:43 +0200

    Fixed index footer && Changed to local SQL Server DBMS

--- GestioneStudenti/Data/ApplicationDbContext.cs
+++ GestioneStudenti/Data/ApplicationDbContext.cs
@@ -6,14 +6,9 @@ namespace GestioneStudenti.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
-        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        //{
-
-        //}
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=185.25.206.105;User ID=SA;Password=database.1374;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
         }
 
 
--- GestioneStudenti/Program.cs
+++ GestioneStudenti/Program.cs
@@ -5,7 +5,10 @@ using GestioneStudenti.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IStorage, SimpleStorage>();
 //builder.Services.AddSingleton<IStorage, JsonStorage>();
